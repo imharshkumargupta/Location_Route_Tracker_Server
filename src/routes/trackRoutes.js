c30a8351eae44120618f24a32073cdd6265ebc06
Harsh Kumar Gupta <imharshkumargupta@gmail.com>
2022-03-14 18:41:35 +0530

    final commit

---
+++ src/routes/trackRoutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireAuth = require("../middleware/requireAuth");
+
+const Track = mongoose.model("Track");
+
+const router = express.Router();
+
+router.use(requireAuth); // this means that every request handler we write here would require signin
+
+router.get("/tracks", async (req, res) => {
+  const tracks = await Track.find({ userId: req.user._id });
+
+  res.send(tracks);
+});
+
+router.post("/tracks", async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    return res.status(422).send({ error: "Must provide a name and locations" });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (err) {
+    res.status(422).send({ error: err.message });
+  }
+});
+
+module.exports = router;
